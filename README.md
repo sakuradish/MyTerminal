f237415bbb19fa7b385ee986cfdb09984243b555
sakuradish <ktok.hobby@gmail.com>
2020-12-21 11:22:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+windo
+[Widget を配置しよう](https://www.shido.info/py/tkinter2.html)
+[Python - Tkinter Text](https://www.tutorialspoint.com/python/tk_text.htm)
---
+++ src/main.py
@@ -0,0 +1,107 @@
+import datetime
+import tkinter as tk
+from tkinter import font
+from tkinter import ttk
+from PIL import Image, ImageTk
+import time
+
+class MemoFrame(tk.Frame):
+    def __init__(self,master,cnf={},**kw):
+        super().__init__(master,cnf,**kw)
+
+        my_font = font.Font(root,family=u'ＭＳ ゴシック',size=14)
+
+        text = tk.Text(self,wrap=tk.CHAR,undo=True, bg='black',font=my_font, foreground='white', insertbackground='white')
+        x_sb = tk.Scrollbar(self,orient='horizontal')
+        y_sb = tk.Scrollbar(self,orient='vertical')
+
+        label1 = tk.Label(self, text='メインタスク')
+        fruits = ['Apple', 'Banana', 'Grape']
+        v1 = tk.StringVar()
+        cb1 = ttk.Combobox(
+            self, textvariable=v1,
+            values=fruits)
+        label2 = tk.Label(self, text='サブタスク')
+        fruits = ['Apple', 'Banana', 'Grape']
+        v2 = tk.StringVar()
+        cb2 = ttk.Combobox(
+            self, textvariable=v2,
+            values=fruits)
+        label3 = tk.Label(self, text='メモ')
+        fruits = ['Apple', 'Banana', 'Grape']
+        v3 = tk.StringVar()
+        cb3 = ttk.Combobox(
+            self, textvariable=v3,
+            values=fruits)
+
+        x_sb.config(command=text.xview)
+        y_sb.config(command=text.yview)
+        text.config(xscrollcommand=x_sb.set,yscrollcommand=y_sb.set)
+
+        text.place(relx=0,rely=0,relwidth=0.97,relheight=0.77)
+        x_sb.place(relx=0,rely=0.77,relwidth=0.97,relheight=0.03)
+        y_sb.place(relx=0.97,rely=0,relwidth=0.03,relheight=0.77)
+        label1.place(relx=0,rely=0.85,relwidth=0.2,relheight=0.05)
+        cb1.place(relx=0.2,rely=0.85,relwidth=0.8,relheight=0.05)
+        label2.place(relx=0,rely=0.9,relwidth=0.2,relheight=0.05)
+        cb2.place(relx=0.2,rely=0.9,relwidth=0.8,relheight=0.05)
+        label3.place(relx=0,rely=0.95,relwidth=0.2,relheight=0.05)
+        cb3.place(relx=0.2,rely=0.95,relwidth=0.8,relheight=0.05)
+
+        self.columnconfigure(0,weight=1)
+        self.rowconfigure(0,weight=1)
+        self.text = text
+        self.x_sb = x_sb
+        self.y_sb = y_sb
+        self.label1 = label1
+        self.cb1 = cb1
+        self.label2 = label2
+        self.cb2 = cb2
+        self.label3 = label3
+        self.cb3 = cb3
+
+        f = open('../data/new.txt','r')
+        lines = f.readlines()
+        f.close()
+        self.text.delete('1.0','end')
+        for line in lines:
+            self.text.insert('end',line)
+        print(self.text.get('1.0','end'))
+
+# ウィンドウ作成
+root = tk.Tk()
+root.title("MyTerminal")
+root.minsize(root.winfo_screenwidth(), root.winfo_screenheight())
+root.state("zoomed")
+
+memoframe = MemoFrame(root)
+memoframe.place(relx=0,rely=0,relwidth=0.5,relheight=1)
+isKeyEventProcessing = False
+def OnKeyEvent(event):
+    global isKeyEventProcessing
+    if isKeyEventProcessing == False:
+        if event.keysym == 'Return' and memoframe.text != root.focus_get():
+            isKeyEventProcessing = True
+            print(event)
+            with open("../data/new.txt", "w") as f:
+                f.write(memoframe.text.get('1.0','end'))
+                dt = datetime.datetime.now()
+                date = dt.strftime("%Y/%m/%d") + "\t" + dt.strftime('%a') + "\t" + dt.strftime('%X')
+                f.write(date + "\t" + memoframe.cb1.get() + "\t" + memoframe.cb2.get() + "\t" + memoframe.cb3.get())
+            f = open('../data/new.txt','r')
+            lines = f.readlines()
+            f.close()
+            memoframe.text.delete('1.0','end')
+            for line in lines:
+                root.update()
+                memoframe.text.insert('end',line)
+                memoframe.text.see('end')
+            isKeyEventProcessing = False
+def OnMouseEvent(event):
+    print(event)
+
+root.bind("<Key>", OnKeyEvent)
+root.bind("<Button>", OnMouseEvent)
+
+# メインループ
+root.mainloop()
